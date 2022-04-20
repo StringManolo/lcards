844ed4b8350ab7cb089a3c47d83bcb773cb1e4f6
stringmanolo <manuelvarelacaldas@gmail.com>
2022-04-20 17:37:37 +0200

    Upload example of material Button

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,7 +1,13 @@
 import * as React from "react";
+import Button from "@material-ui/core/Button";
 
 const Test = () => (
-  <h1>Test</h1>
+  <>
+    <h1>Test</h1>
+    <Button variant="contained" color="primary">
+      Click Me
+    </Button>
+  </>
 )
 
 export default Test;
